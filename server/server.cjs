2f917ee41dd935d8ebb4b129c26a4cc517bc2a50
dennohkim <kimathidennis38@gmail.com>
2023-02-14 21:05:54 +0300

    express framework intro

--- server/server.cjs
+++ server/server.cjs
@@ -1,12 +1,35 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
-
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("^/$|/index(.html)?", (req, res) => {
+  //res.sendFile('./views/index.html', {root: __dirname});
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html");
+});
+
+//Route Handlers
+//Function chaining together
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next();
+}, ( req, res) => {
     res.send('Hello, world')
+})
+
+app.get('/*', (req, res) => {
+      res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+
+
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
